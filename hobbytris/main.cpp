309b4b4728c389517c387c264037cb0397d16fa2
Slapjacky <jerikolo1@gmail.com>
2020-02-18 15:32:56 +0100

    Menu v1.0

--- hobbytris/main.cpp
+++ hobbytris/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-	cout << "Hello World!" << endl;
+	int menuChoice;
+	bool isInMenu = true;
+
+	// Run till player has chosen a valid option
+	do {
+		std::cout << "Welcome to hobbytris. Enter the option's number to continue." << std::endl;
+		std::cin >> menuChoice;
+
+		if (menuChoice == 1)
+		{
+			std::cout << "\033[2J\033[u"; // Clears screen, resets cursor
+			std::cout << "You've chosen play!" << std::endl;
+			isInMenu = false;
+		}
+		else if (menuChoice == 2)
+		{
+			std::cout << "\033[2J\033[u";
+			std::cout << "You've chosen quit!" << std::endl;
+			isInMenu = false;
+		}
+		else if (menuChoice != 1 || 2)
+		{
+			std::cout << "\033[2J\033[u";
+			std::cout << "Please enter a valid option." << std::endl;
+		}
+	}
+
+	while (isInMenu == true);
+
 	return 0;
 }
+
