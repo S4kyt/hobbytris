51ad60556a3d810ba21e9293c385c75239121941
Slapjacky <jerikolo1@gmail.com>
2020-02-18 23:32:17 +0100

    Fixed menu's validation

--- hobbytris/main.cpp
+++ hobbytris/main.cpp
@@ -8,6 +8,8 @@ int main()
 	// Run till player has chosen a valid option
 	do {
 		std::cout << "Welcome to hobbytris. Enter the option's number to continue." << std::endl;
+		std::cout << "1. Play" << std::endl;
+		std::cout << "2. Quit" << std::endl;
 		std::cin >> menuChoice;
 
 		if (menuChoice == 1)
@@ -22,7 +24,7 @@ int main()
 			std::cout << "You've chosen quit!" << std::endl;
 			isInMenu = false;
 		}
-		else if (menuChoice != 1 || 2)
+		else
 		{
 			std::cout << "\033[2J\033[u";
 			std::cout << "Please enter a valid option." << std::endl;
